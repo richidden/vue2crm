e738f1bcdc2a91be49b963cbe4c7d51cbe647981
Richard <richard.cuela@vipfe.gob.bo>
2018-04-17 08:36:20 -0400

    validation change  the form

--- src/pages/CustomerForm.vue
+++ src/pages/CustomerForm.vue
@@ -15,28 +15,30 @@
         </v-card-title>
         <v-card-text>
           <v-container fluid grid-list-md>
-            <v-layout row wrap>
-              <v-flex md4 xs12>
-                <v-text-field name="firstName" label="First Name" hint="Last name is required" value="Input text" v-model="customer.firstName" class="input-group--focused" required></v-text-field>
-              </v-flex>
-              <v-flex md4 xs12>
-                <v-text-field name="lastName" label="Last Name" maxlength="10" hint="Last name is required" value="Input text" v-model="customer.lastName" class="input-group--focused" required></v-text-field>
-              </v-flex>
-              <v-flex md4 xs12 v-if="customer.avatar">
-                <img v-if="customer.avatar" class="responsive" v-bind:src="customer.avatar"/>
-              </v-flex>
-              <v-flex md4 xs12>
-                <v-text-field name="age" type="number" label="Age" hint="Number between 18 to 150" v-bind:rules="rules.age"
-                 v-model="customer.age" class="input-group--focused" required></v-text-field>
-              </v-flex>
-              <v-flex md4 xs12>
-                <v-text-field name="email" type="email" label="Email" value="Input text" v-model="customer.email"
-                 v-bind:rules="rules.email" class="input-group--focused" required></v-text-field>
-              </v-flex>
-               <v-flex md4 xs12>
-                  <v-switch label="Customer Status"  v-model="customer.isActive" light></v-switch>
-              </v-flex>
-            </v-layout>
+            <v-form v-model="valid" ref="form" lazy-validation>
+              <v-layout row wrap>
+                <v-flex md4 xs12>
+                  <v-text-field name="firstName" label="First Name" hint="Last name is required" value="Input text" v-model="customer.firstName" class="input-group--focused" required></v-text-field>
+                </v-flex>
+                <v-flex md4 xs12>
+                  <v-text-field name="lastName" label="Last Name" maxlength="10" hint="Last name is required" value="Input text" v-model="customer.lastName" class="input-group--focused" required></v-text-field>
+                </v-flex>
+                <v-flex md4 xs12 v-if="customer.avatar">
+                  <img v-if="customer.avatar" class="responsive" v-bind:src="customer.avatar"/>
+                </v-flex>
+                <v-flex md4 xs12>
+                  <v-text-field name="age" type="number" label="Age" hint="Number between 18 to 150" v-bind:rules="rules.age"
+                   v-model="customer.age" class="input-group--focused" required></v-text-field>
+                </v-flex>
+                <v-flex md4 xs12>
+                  <v-text-field name="email" type="email" label="Email" value="Input text" v-model="customer.email"
+                   v-bind:rules="rules.email" class="input-group--focused" required></v-text-field>
+                </v-flex>
+                 <v-flex md4 xs12>
+                    <v-switch label="Customer Status"  v-model="customer.isActive" light></v-switch>
+                </v-flex>
+              </v-layout>
+            </v-form>
           </v-container>
         </v-card-text>
       </v-card>
@@ -47,19 +49,20 @@
 export default {
   data () {
     return {
+      valid: true,
       errors: [],
       title: '',
       customer: {},
       rules: {
-        age: [() => {
-          if (this.customer.age < 18 || this.customer.age > 100) return 'Age is required. It must be bewteen 18 and 100'
-        }],
-        email: [() => {
-          if (this.customer.email) {
-            /* eslint-disable no-useless-escape */
-            let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            if (!re.test(this.customer.email)) return 'Email is invalid.'
-          }
+        age: [
+          v => !!v || 'Age is required',
+          v => (v < 18 || v > 100) || 'Age is required. It must be bewteen 18 and 100'
+          ],
+        email: [(v) => {
+          /* eslint-disable no-useless-escape */
+          let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          if (!re.test(this.customer.email)) return 'mail is invalid.'
+          else return true
         }]
       }
     }
@@ -68,19 +71,21 @@ export default {
   },
   methods: {
     save () {
-      let customer = this.customer
-      if (!customer.id) {
-        this.api.postData('customers', customer).then((res) => {
-          this.$router.push({name: 'Customers'})
-        }, (err) => {
-          console.log(err)
-        })
-      } else {
-        this.api.putData('customers/' + customer.id.toString(), customer).then((res) => {
-          this.$router.push({name: 'Customers'})
-        }, (err) => {
-          console.log(err)
-        })
+      if (this.$refs.form.validate()) {
+        let customer = this.customer
+        if (!customer.id) {
+          this.api.postData('customers', customer).then((res) => {
+            this.$router.push({name: 'Customers'})
+          }, (err) => {
+            console.log(err)
+          })
+        } else {
+          this.api.putData('customers/' + customer.id.toString(), customer).then((res) => {
+            this.$router.push({name: 'Customers'})
+          }, (err) => {
+            console.log(err)
+          })
+        }
       }
     },
     getById () {
